6c222ae2561e50d618116b109eae357acaf5b2a8
Morgan-Wright <morgan.q.wright1217@gmail.com>
2022-03-12 10:11:14 -0800

    problem 2 complete

--- arrayMethods.js
+++ arrayMethods.js
@@ -41,9 +41,13 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
+// const postTaxPrices = prices.map(function(tax) {
+//   return tax * 1.07
+// })
 
+// const postTaxPrices = prices.map(tax => tax * 1.07 )
 
-
+// console.log(postTaxPrices)
 
 ////////// PROBLEM 3 //////////
 
